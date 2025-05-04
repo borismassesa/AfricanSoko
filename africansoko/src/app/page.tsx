188a50e3c64c0596f229e3c305b15e53adf58113
Boris Massesa <104251801+borismassesa@users.noreply.github.com>
2025-05-03 22:01:11 -0700

    Customization

--- africansoko/src/app/page.tsx
+++ africansoko/src/app/page.tsx
@@ -1,11 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-2xl font-bold text-rose-500">
-        Hello Boris 
-      </p>
+    <div className="p-6">
+    <div className="flex flex-col gap-y-5">
+      <div>
+        <Button variant="elevated">
+          I am a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder=" I am an input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="I am a textarea" />
+      </div>
+    </div>
     </div>
 
   )
